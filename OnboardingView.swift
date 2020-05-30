e172e518828c2497499e07aaa057a41bd1cf2993
premdhoot <premdhoot6@gmail.com>
2020-05-30 14:24:12 -0700

    Onboarding View CocoaPod

---
+++ OnboardingView.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingView.swift
+//  COVID
+//
+//  Created by Prem Dhoot on 5/30/20.
+//  Copyright © 2020 Prem Dhoot. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnboardingView: PaperOnboarding {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
