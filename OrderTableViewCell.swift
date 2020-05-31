1d812142334880e381c9ae922ed1775ed88fa6d8
premdhoot <premdhoot6@gmail.com>
2020-05-30 18:58:11 -0700

    Cell for volunteer order list

---
+++ OrderTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  OrderTableViewCell.swift
+//  COVID
+//
+//  Created by Prem Dhoot on 5/30/20.
+//  Copyright © 2020 Prem Dhoot. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var orderEmailName: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
